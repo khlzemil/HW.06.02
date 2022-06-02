ccdd97ac8150c8b238a2fde07be1c0dcc77fdbc5
khlzemil <emilmg@code.edu.az>
2022-06-02 11:56:59 +0400

    Added HomeTask

---
+++ HW.06.02/HW.06.02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW._06._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Fullname = "Emil Quluzade";
+            user.Email = "emilquluzade@gmail.com";
+            user.Password = "Salam4321";
+            ShowInfo(user);
+
+
+            User user1 = new User();
+            user1.Fullname = "Tural Ismayilov";
+            user1.Email = "ismayilov@gmail.com";
+            user1.Password = "Salam4321";
+            ShowInfo(user1);
+
+        }
+
+        static void ShowInfo(User user)
+        {
+            Console.WriteLine(user.PasswordChecker());
+            Console.WriteLine(user.Fullname);
+            Console.WriteLine(user.Email);
+
+            
+        }
+    }
+}
---
+++ HW.06.02/HW.06.02/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._06._02
+{
+    internal class User:IAccount
+    {
+        public string Fullname;
+        public string Email;
+        public string Password;
+
+        public bool PasswordChecker()
+        {
+            
+            char[] checkUpperLetter = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] checkLowerLetter = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] checkNumber = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            bool flagForNumber = false;
+            bool flagForUpperLetter = false;
+            bool flagForLowerLetter = false;
+            
+            for (int i = 0; i < Password.Length; i++)
+            {
+                for (int j = 0; j < checkNumber.Length; j++)
+                {
+                    if (Password[i] == checkNumber[j])
+                    {
+                        flagForNumber = true;
+                        break;
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < Password.Length; i++)
+            {
+                for (int j = 0; j < checkUpperLetter.Length; j++)
+                {
+                    if (Password[i] == checkUpperLetter[j])
+                    {
+                        flagForUpperLetter = true;
+                        break;
+
+                    }
+                }
+            }
+
+            for (int i = 0; i < Password.Length; i++)
+            {
+                for (int j = 0; j < checkLowerLetter.Length; j++)
+                {
+                    if (Password[i] == checkLowerLetter[j])
+                    {
+                        flagForLowerLetter = true;
+                        break;
+
+                    }
+                }
+            }
+
+            if (flagForNumber == true && flagForLowerLetter == true && flagForUpperLetter == true && Password.Length >= 8)
+            {
+                return true;
+            }
+            else
+                return false;
+
+            
+        }
+    }
+}
